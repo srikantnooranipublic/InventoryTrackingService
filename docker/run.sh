3b0c82c4ff77373bc643af58b206ec5fcbdb2346
Srikant <srikns@yahoo.com>
2018-12-08 01:07:05 -0500

    bug fix

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 docker run -d \
 -p 8999:8080 \
 --add-host=centOS7.5-aop2.dev.fco:10.238.32.21 \
--v /opt/ca/JenkinsPipelineApp/DockerApp/APP_RUNTIME/logs:/opt/ca/springbootApp/wily/logs \
+-v /Users/noosr03/Documents/CA_Technical/2_ProjectRepo/InventoryTrackingService/docker/APP_RUNTIME/logs:/opt/ca/springbootApp/wily/logs \
 --name=apm-agent \
 apm-agent:latest
