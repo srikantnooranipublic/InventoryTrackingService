f07280a0fc29310cc9a5c305eaf6cdb783c9ca6e
Srikant <srikns@yahoo.com>
2018-12-09 23:38:56 -0500

    fix couple of files

--- docker/app/dockerAppStartup.sh
+++ docker/app/dockerAppStartup.sh
@@ -6,6 +6,8 @@ PWD_NEW=`echo $PWD | sed 's_/_\\\\/_g'`
 sed 's/RUNTIME_FOLDER/'$PWD_NEW'/g' run.sh > run.sh.changed
 /bin/mv -f run.sh.changed run.sh
 
+chmod 755 run.sh
+
 
 echo "Docker App cleanup"
 
