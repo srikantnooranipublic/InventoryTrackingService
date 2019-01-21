3a56dfe993e8f7661061c4ef66c09011ab8ab23f
Srikant <srikns@yahoo.com>
2019-01-21 16:13:28 -0500

    fixed folder path

--- docker/app/run.sh
+++ docker/app/run.sh
@@ -1,5 +1,5 @@
 docker run -d \
 -p 8999:8080 \
--v /Users/noosr03/Documents/CA_Technical/2_ProjectRepo/InventoryTrackingService/docker/app/logs:/opt/ca/springbootApp/wily/logs \
+-v RUNTIME_FOLDER/logs:/opt/ca/springbootApp/wily/logs \
 --name=apm-agent \
 apm-agent:latest
